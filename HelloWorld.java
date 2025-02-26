655913e36a026522359908da8248bb2719b42c64
Marcos Souza <152660845+Marcos-Souz4@users.noreply.github.com>
2025-02-26 20:10:11 -0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Estou escrevendo isso para imprimir algo diferente de");
+    System.out.println("Hello World!");
   }
 }
