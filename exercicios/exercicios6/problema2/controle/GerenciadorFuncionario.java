4b4add0ab953860a7a3098f5aff76593edf9e981
Marcos Souza <marcosfdesouza13@gmail.com>
2025-04-15 19:27:24 -0300

    exercicio 2 da lista exercicios6

--- exercicios/exercicios6/problema2/controle/GerenciadorFuncionario.java
+++ exercicios/exercicios6/problema2/controle/GerenciadorFuncionario.java
@@ -1,5 +1,35 @@
 package exercicios6.problema2.controle;
 
+import exercicios6.problema2.dominio.*;
+import java.util.Scanner;
+
 public class GerenciadorFuncionario {
-  
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Digite o nome do funcionário: ");
+    String nome = sc.nextLine();
+
+    System.out.println("Digite seu salário base: R$");
+    double salario = sc.nextDouble();
+    sc.nextLine();
+
+    if(salario<=0) {
+      System.out.println("Salário Inexistente!");
+      System.exit(1);
+    }
+
+    System.out.println("Digite sua categoria: ");
+    String categoria = sc.nextLine();
+    
+    Funcionario funcionario = new Funcionario(nome, salario, categoria);
+
+    System.out.println("Exibindo as informações do funcionário: ");
+    funcionario.listarFuncionario();
+
+    funcionario.calcularSalarioFinal();
+
+    sc.close();
+  }
 }
--- exercicios/exercicios6/problema2/dominio/Funcionario.java
+++ exercicios/exercicios6/problema2/dominio/Funcionario.java
@@ -11,15 +11,75 @@ Requisitos:
 ▪ "A" → +20%
 ▪ "B" → +10%
 ▪ "C" → sem bônus
+* Utilizar switch-case para aplicar a regra do bônus. 
 
 2. Criar o pacote controle contendo a classe GerenciadorFuncionario:
-o Permitir que o usuário cadastre um funcionário e consulte seu
-salário final.
-* Utilizar switch-case para aplicar a regra do bônus. */
+Permitir que o usuário cadastre um funcionário e consulte seu
+salário final. */
 
 package exercicios6.problema2.dominio;
 
 public class Funcionario {
  
+  private String nome;  //declarando as variáveis
+  private double salarioBase;
+  private String categoria;
+
+  public Funcionario(String nome, double salarioBase, String categoria) {  //método construtor 
+    this.nome = nome;
+    this.salarioBase = salarioBase;
+    this.categoria = categoria;
+  }
+
+  public String getNome() {  //"getters and setters"
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public double getSalarioBase() {
+    return salarioBase;
+  }
+
+  public void setSalarioBase(double salarioBase) {
+    this.salarioBase = salarioBase;
+  }
+
+  public String getCategoria() {
+    return categoria;
+  }
+
+  public void setCategoria(String categoria) {
+    this.categoria = categoria;
+  }
   
+  public void listarFuncionario() {
+    System.out.println("\nNome: " + nome);
+    System.out.println("Salário Base: R$" + salarioBase);
+    System.out.println("Categoria: " + categoria + "\n");
+  }
+
+  public void calcularSalarioFinal() {
+
+    switch (categoria) {
+      case "A":
+        salarioBase +=  salarioBase * 0.2;
+        System.out.println("Bônus de 20% adicionado ao seu sálario base! Salário Atual: R$" + salarioBase);
+        break;
+
+      case "B":
+        salarioBase +=  salarioBase * 0.1;
+        System.out.println("Bônus de 10% adicionado ao seu sálario base! Salário Atual: R$" + salarioBase);
+        break;
+
+      case "C":
+        System.out.println("Nenhum bônus adicionado ao seu salário base! Salário atual: R$" + salarioBase);
+        break;
+        
+      default:
+        System.out.println("Categoria inexistente...");
+    }
+  }
 }
