c61eb8a16c46b34d7676ce62297cb73a7f758db5
Marcos Souza <marcosfdesouza13@gmail.com>
2025-06-02 23:44:31 -0300

    commit desesperado

---
+++ exercicios/exerciciosExceções/problema1/ConversorTemperatura.java
@@ -0,0 +1,10 @@
+// Exercício 1: Conversor de Temperatura
+// Crie um programa que leia uma temperatura em Celsius informada pelo usuário e a converta para Fahrenheit.
+// Trate a possibilidade de entrada inválida (valores não numéricos) usando try-catch.
+
+package exerciciosExceções.problema1;
+
+public class ConversorTemperatura {
+ 
+  
+}
